8e3454f47f421ded83328bf99a6fbd7468d9579f
k-hench <khench@posteo.net>
2022-08-12 17:17:52 +0200

    init

---
+++ R/theme_kh.R
@@ -0,0 +1,33 @@
+fnt_sans <- "Josefin Sans Light"
+fnt_serif <- "TeX Gyre Pagella"
+
+theme_set(
+  theme_minimal(base_family = fnt_serif,
+                base_size = 14) +
+    theme(title = element_text(family = fnt_sans,
+                               face = "bold"),
+          plot.caption =  element_text(family = fnt_serif,
+                                       face = "italic"),
+          panel.grid = element_blank(),
+          plot.tag = element_text(family = fnt_serif,
+                                  face = "italic"),
+          axis.ticks = element_line(size = .2),
+          axis.ticks.length = unit(-4, 'pt'),
+          panel.spacing = unit(0,"pt"),
+          plot.background = element_blank(),
+          panel.background = element_blank())
+)
+
+clr_light <- "gray85"
+clr_def <- "gray50"
+fll_def <- "gray95"
+sz_def <- .4
+
+def_list <- list(colour = clr_def,
+                 fill = fll_def,
+                 size = sz_def)
+
+c("rect", "point",
+  "line", "boxplot") |> 
+  walk(update_geom_defaults,
+       new = def_list)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Antarctic Fur Seal Genetic Load <img src="img/arcgaz_plain.svg" align="right" alt="" width="120" />
+
+**Author:** Kosmas Hench
